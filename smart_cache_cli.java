ac6c561ea53f9c4ce95f46ec4b7357851afd952e
jreleaserbot <jreleaser@kordamp.org>
2023-07-11 17:34:11 +0000

    smart-cache-cli v0.0.7

---
+++ smart_cache_cli.java
@@ -0,0 +1,10 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 17+
+//DEPS com.redis:smart-cache-cli:0.0.7
+// Generated with JReleaser 1.8.0-SNAPSHOT at 2023-07-11T17:33:54.176390637Z
+
+public class smart_cache_cli {
+    public static void main(String... args) throws Exception {
+        com.redis.smartcache.cli.Application.main(args);
+    }
+}
